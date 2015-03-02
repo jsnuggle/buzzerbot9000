303b9bd6f35dc1deb7124cc99b4aefe6dbdfa38f
Jeff Clarke <jeff.clarke@gmail.com>
2015-03-02 03:48:41 +0000

    Refactored GPIO to use onoff; Added doorbell lib; Renamed buzzer to 'opener' to avoid confusion; WORKING

--- server.js
+++ server.js
@@ -1,36 +1,44 @@
 "use strict";
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var config = require('configure');
 var twilio = require('twilio');
 
-var Buzzer = require('./lib/buzzer.js');
+var Opener = require('./lib/opener.js');
+var Doorbell = require('./lib/doorbell.js');
 var Validators = require('./lib/validators.js');
 
 process.env.TWILIO_AUTH_TOKEN = config.twilio_token;
 
 var app = express(),
-    buzzer = new Buzzer(config, new Validators(config));
+    opener = new Opener(config, new Validators(config)),
+    doorbell = new Doorbell(config);
 
 console.log('Starting...');
 
-app.get('/sms', twilio.webhook(), function (request, response) {
-    console.log(request.query);
+doorbell.listen();
 
-    var sms = request.query;
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    buzzer.open_door(sms.Body, sms.From, function (message) {
+app.post('/sms', twilio.webhook(), function (request, response) {
+
+    var sms = request.body;
+
+    opener.open_door(sms.Body, sms.From, function (success) {
         var twil_res = new twilio.TwimlResponse();
-
-        twil_res.message(message || "???");
+        var message = success ? config.success_message : config.fail_message;
+        twil_res.message(message);
         console.log('"' + message + '"', sms.Body, sms.From);
 
-        response.send(twil_res);
+        if (config.send_reply) {
+            response.send(twil_res);
+        }
     });
 
 });
 
 var port = Number(config.port);
 app.listen(port, function () {
-    console.log('Listening on ' + port);
+    console.log('Listening for the opener signal via HTTP on port ' + port);
 });
